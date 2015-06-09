c0034ee270382c8e19d8dbcc6d67468258e8daef
Claus Dahl <dee@classy.dk>
2015-06-09 18:06:05 +0200

    working extraction from etnul

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'sinatra', '>=1.3.1'
+gem 'json'
+gem 'shotgun'
---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'json'
+require 'digest'
+require 'base64'
+
+post '/img/' do
+  if params[:uri]
+    raw = params[:uri]
+    bits = Base64.decode64(raw[22..-1])
+    digest = Base64.encode64(Digest::SHA256.digest(bits)).chomp.gsub(/[^a-zA-Z0-9]/,'')
+    filename = digest + '.png'
+    File.open(File.dirname(__FILE__) + '/public/cache/' + filename, 'w') do |f|
+      f.write(bits)
+    end
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    content_type :json
+    { :url => request.url.gsub(/img.?$/, '') + 'cache/' + filename}.to_json
+  end
+end
